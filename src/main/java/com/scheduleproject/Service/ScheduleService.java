b87b7a569d33243918fd35d36625edb97a36c614
Kingyul Kim <youremail@example.com>
2025-11-06 10:24:13 +0900

    ScheduleService createSchedule 객체 주석 추가 202511061024

--- src/main/java/com/scheduleproject/Service/ScheduleService.java
+++ src/main/java/com/scheduleproject/Service/ScheduleService.java
@@ -13,6 +13,11 @@ import org.springframework.transaction.annotation.Transactional;
 public class ScheduleService {
     private final ScheduleRepository scheduleRepository;
 
+    /**
+     * 스케줄 테이블 생성
+     * @param createScheduleRequest CreateScheduleRequest 에 있는 json 형태의 입력값 파라미터 
+     * @return api에 보여줄 CreateScheduleResponse 반환
+     */
     @Transactional
     public CreateScheduleResponse createSchedule(CreateScheduleRequest createScheduleRequest) {
         Schedule schedule = new Schedule(
